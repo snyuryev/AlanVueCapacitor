28d69126cfdcb4fd94ff02182455b66c11425a38
Sergey Yuryev <snyuryev@gmail.com>
2022-04-25 11:32:04 +0300

    initial ios

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="Tab 11 page" />
+       <alan-button  alan-key="4901cecdb3e095fd56b1cbd1c96e3cc02e956eca572e1d8b807a3e2338fdd0dc/stage" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  defineComponent({
+  name: 'Tab1Page',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
