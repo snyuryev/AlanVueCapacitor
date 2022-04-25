e173fa2aa85d5d89ebdcba8d9d8f96f68f7e199a
Sergey Yuryev <snyuryev@gmail.com>
2022-04-25 17:18:38 +0300

    onCommand

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -13,7 +13,7 @@
       </ion-header>
     
       <ExploreContainer name="Tab 11 page" />
-       <alan-button  alan-key="4901cecdb3e095fd56b1cbd1c96e3cc02e956eca572e1d8b807a3e2338fdd0dc/stage" />
+       <alan-button v-on:command="command" alan-key="678f812efb0f25b956b1cbd1c96e3cc02e956eca572e1d8b807a3e2338fdd0dc/stage" />
     </ion-content>
   </ion-page>
 </template>
@@ -25,6 +25,11 @@ import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+   methods: {
+      command: function(data:any) {
+        console.info('Received command: ', data.detail);
+      }
+    },
 });
 </script>
